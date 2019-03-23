a6a80a7852a0384183b1f6199c244dc286698545
Rayner Mendez <raynermendezg@gmail.com>
2019-03-23 14:50:55 -0400

    First commit

---
+++ src/main/java/com/portfolio/repositories/ContactMeRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.repositories;
+
+import com.portfolio.models.ContactMe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactMeRepository {
+}
