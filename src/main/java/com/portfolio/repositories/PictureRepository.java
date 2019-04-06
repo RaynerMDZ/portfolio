389a63c2348bae6cda4d193a2db8b30b35feefcd
Rayner Mendez <raynermendezg@gmail.com>
2019-04-06 02:03:32 -0400

    Picture and Portfolio repositories added.

---
+++ src/main/java/com/portfolio/repositories/PictureRepository.java
@@ -0,0 +1,9 @@
+package com.portfolio.repositories;
+
+import com.portfolio.entities.PictureEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<PictureEntity, Long> {
+}
