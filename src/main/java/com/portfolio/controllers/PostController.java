61f691df71fe30ea0b5e1266cfa8f8dc68135c77
Rayner Mendez <raynermendezg@gmail.com>
2019-04-12 22:34:39 -0400

    Post Controller modified. Link reference to images in portfolio section of index.html.

--- src/main/java/com/portfolio/controllers/PostController.java
+++ src/main/java/com/portfolio/controllers/PostController.java
@@ -4,10 +4,6 @@ import com.portfolio.services.PictureService;
 import com.portfolio.services.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
@@ -26,30 +22,5 @@ public class PostController {
     this.portfolioPostService = portfolioPostService;
     this.pictureService = pictureService;
     this.commentController = commentController;
-  }
-
-  /**
-   *
-   * @param model
-   * @return String
-   */
-  @GetMapping
-  @RequestMapping({"/portfolio"})
-  public String getAllPost(Model model) {
-
-    model.addAttribute("posts", portfolioPostService.getAllPosts());
-    return "fragments/portfolio/portfolio";
-  }
-
-  /**
-   *
-   * @param Id
-   * @return String
-   */
-  @GetMapping
-  @RequestMapping("/post")
-  public String getPostById(@PathVariable Long Id) {
-
-    return null;
   }
 }
